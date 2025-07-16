010e4202e273336209a6c366d16987cbbdf70e12
jimi1111 <jahnawimani@gmail.com>
2025-07-16 15:14:42 +0530

    Revert "Added sub zone in zoning page"
    This reverts commit b9b18069d1d274cb90113b8d715de5f1361f7fc9.

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppWrapper from './App'; // <--- CHANGE THIS LINE: Import AppWrapper instead of App
+import App from './App';
 import './index.css'; // Import global styles including Tailwind and custom CSS
 
 // Create a root to render the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the main AppWrapper component into the root
+// Render the main App component into the root
 root.render(
   <React.StrictMode>
-    <AppWrapper /> {/* <--- CHANGE THIS LINE: Render AppWrapper instead of App */}
+    <App />
   </React.StrictMode>
 );
